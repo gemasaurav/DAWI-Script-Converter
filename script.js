6da0ea0787918f78e4bf9ea33ab60c1806f07cea
gemasaurav <gema.saurav@gmail.com>
2026-06-28 09:08:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Show Date Screen after 3 seconds
+setTimeout(function(){
 
+    document.getElementById("splash").style.display="none";
+    document.getElementById("dateScreen").style.display="block";
+
+},3000);
+
+document.getElementById("nextBtn").addEventListener("click",function(){
+
+    let date=document.getElementById("codingDate").value;
+
+    if(date.length!=10){
+        alert("Enter date in DD/MM/YYYY format");
+        return;
+    }
+
+    // Hidden calculation will be added later
+
+    alert("Converter Page Coming Next...");
+});
