2480cd571b65260f891cb8c06e576732b908fdca
gemasaurav <gema.saurav@gmail.com>
2026-06-28 09:25:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,31 @@ document.getElementById("nextBtn").addEventListener("click",function(){
 
     // Hidden calculation will be added later
 
-    window.location.href = "converter.html";
+    // Remove all / characters
+let digits = date.replace(/\//g, "");
+
+// Calculate digit sum
+let sum = 0;
+
+for (let i = 0; i < digits.length; i++) {
+    sum += parseInt(digits[i]);
+}
+
+// Convert to a single digit
+while (sum > 9) {
+    let temp = 0;
+
+    while (sum > 0) {
+        temp += sum % 10;
+        sum = Math.floor(sum / 10);
+    }
+
+    sum = temp;
+}
+
+// Save the hidden code
+localStorage.setItem("dawiCode", sum);
+
+// Open converter page
+window.location.href = "converter.html";
 });
